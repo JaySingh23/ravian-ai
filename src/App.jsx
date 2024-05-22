141c72c848272eacb4e167ba7dad5c5f9dfb8f66
JaySingh <jaysingh1452@gmail.com>
2024-05-22 19:09:50 +0530

    added responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -60,21 +60,43 @@ const App = () => {
           duration: 0.5
         })
 
-      gsap.fromTo(servicesRef.current.children, {
-        opacity: 0,
-        y: 50,
-      }, {
-        opacity: 1,
-        y: 0,
-        duration: 0.8,
-        stagger: 0.2,
-        scrollTrigger: {
-          trigger: servicesRef.current,
-          start: "top 100%",
-          end: "bottom 100%",
-          scrub: true,
-        }
-      });
+      // Check if the screen size matches the small breakpoint (sm)
+      const isSmallScreen = window.matchMedia('(max-width: 640px)').matches;
+
+      // ScrollTrigger animation configuration for large screens
+      if (!isSmallScreen) {
+        gsap.fromTo(servicesRef.current.children, {
+          opacity: 0,
+          y: 50,
+        }, {
+          opacity: 1,
+          y: 0,
+          duration: 0.8,
+          stagger: 0.2,
+          scrollTrigger: {
+            trigger: servicesRef.current,
+            start: "top 100%",
+            end: "bottom 90%",
+            scrub: true,
+          }
+        });
+      } else { // ScrollTrigger animation configuration for small screens
+        gsap.fromTo(servicesRef.current.children, {
+          opacity: 0.5,
+          y: 50,
+        }, {
+          opacity: 1,
+          y: 0,
+          duration: 0.8,
+          stagger: 0.2,
+          scrollTrigger: {
+            trigger: servicesRef.current,
+            start: "top 80%",
+            end: "bottom 90%",
+            scrub: true,
+          }
+        });
+      }
 
       gsap.fromTo(visionRef.current.children, {
         opacity: 0,
@@ -135,49 +157,54 @@ const App = () => {
         className="h-screen p-10 bg-gray-50 absolute top-0 justify-center place-items-center font-poppins z-10 w-full flex flex-col gap-10 tracking-tight"
       >
         <video src={Namaste} width="300" height="300" autoPlay={true} muted />
-        <h1 className="text-9xl" id="title-1">
+        <h1 className="text-4xl sm:text-9xl" id="title-1">
           Namaste!
         </h1>
       </div>
       <div className="h-screen bg-white">
         <ParticleBackground ref={particleRef}/>
         <Navbar ref={navbarRef} id="navbar" />
-        <div className="h-screen flex flex-col bg-black justify-center place-items-center z-1">
+        <div className="h-screen flex flex-col bg-black justify-center items-center z-1">
           <h1
             id="welcome"
-            className="text-9xl font-bold text-gray-100 font-poppins z-20"
+            className="text-6xl sm:text-9xl font-bold text-gray-100 font-poppins z-20 "
           >
             Ravian AI
           </h1>
-          <h3 id="tagline" className="text-3xl text-gray-100 font-poppins z-20 align-center">
+          <h3 id="tagline" className="text-lg sm:text-3xl text-gray-100 font-poppins z-20 text-center pl-5 pr-5">
             Turn Data into Decisions, with our AI Data Scientist
           </h3>
         </div>
       </div>
 
       <div className="offerings-section bg-white pl-10 pr-10" ref={servicesRef}>
-        <h1 className="text-8xl font-semibold text-white font-poppins">Our Offerings</h1>
-        <div className="container mx-auto flex flex-row justify-around items-start gap-10 pt-10">
-          <Atropos activeOffset={40}
+        <h1 className="text-6xl sm:text-8xl font-semibold text-white font-poppins">Our Offerings</h1>
+        <div className="container mx-auto flex flex-col sm:flex-row justify-around items-start gap-10 pt-10">
+        <Atropos
+          activeOffset={40}
           shadowScale={1.05}
-          className="my-atropos bg-gradient-to-r from-gray-950  p-10 rounded-lg transition duration-500 hover:shadow-xl hover:shadow-white cursor-pointer " >
-              <div className="my-atropos-image rounded-lg" data-atropos-offset="5"></div>
-              <h2 className="text-4xl font-bold mb-5 text-white align-center mt-5" data-atropos-offset="5">AI Data Analyst</h2>
-              <p className="text-md text-white text-poppins" data-atropos-offset="5" >Unlock the full potential of your data with RAVIAN AI Data Analyst - an intelligent platform designed to transform raw data into actionable insights. Our AI Data Analyst is equipped with the latest machine learning algorithms and cutting-edge technologies to analyze, interpret and visualize data with precision and speed.</p>
-          </Atropos>
-          <Atropos activeOffset={40}
+          className="w-full sm:w-1/2 my-atropos bg-gradient-to-r from-gray-950 p-10 rounded-lg transition duration-500 hover:shadow-xl hover:shadow-white cursor-pointer"
+        >
+          <div className="my-atropos-image rounded-lg" data-atropos-offset="5"></div>
+          <h2 className="text-4xl font-bold mb-5 text-white align-center mt-5" data-atropos-offset="5">AI Data Analyst</h2>
+          <p className="text-md text-white text-poppins" data-atropos-offset="5">Unlock the full potential of your data with RAVIAN AI Data Analyst - an intelligent platform designed to transform raw data into actionable insights. Our AI Data Analyst is equipped with the latest machine learning algorithms and cutting-edge technologies to analyze, interpret and visualize data with precision and speed.</p>
+        </Atropos>
+        <Atropos
+          activeOffset={40}
           shadowScale={1.05}
-          className="my-atropos bg-gradient-to-r from-gray-950  p-10 rounded-lg transition duration-500 hover:shadow-xl hover:shadow-white cursor-pointer " >
-              <div className="my-atropos-image-1 rounded-lg" data-atropos-offset="-5"></div>
-              <h2 className="text-4xl font-bold mb-5 text-white align-center mt-5" data-atropos-offset="-5">Gen AI Vision Analyst</h2>
-              <p className="text-md text-white text-poppins" data-atropos-offset="-5" >Introducing the Gen AI Vision Analyst: Redefining Visual Intelligence at RAVIAN AI. We are excited to introduce our revolutionary GenAI Vision Analyst - a next generation solution that sets a new standard for visual intelligence and data analysis.</p>
-          </Atropos>
-        </div>
+          className="w-full sm:w-1/2 my-atropos bg-gradient-to-r from-gray-950 p-10 rounded-lg transition duration-500 hover:shadow-xl hover:shadow-white cursor-pointer"
+        >
+          <div className="my-atropos-image-1 rounded-lg" data-atropos-offset="-5"></div>
+          <h2 className="text-4xl font-bold mb-5 text-white align-center mt-5" data-atropos-offset="-5">Gen AI Vision Analyst</h2>
+          <p className="text-md text-white text-poppins" data-atropos-offset="-5">Introducing the Gen AI Vision Analyst: Redefining Visual Intelligence at RAVIAN AI. We are excited to introduce our revolutionary GenAI Vision Analyst - a next generation solution that sets a new standard for visual intelligence and data analysis.</p>
+        </Atropos>
+      </div>
+
 
         <div className="vision-section pt-40 pb-20 " ref={visionRef}>
-          <h1 className="text-8xl font-semibold text-white font-poppins mb-10">Our Vision</h1>
+          <h1 className="text-6xl sm:text-8xl font-semibold text-white font-poppins mb-10">Our Vision</h1>
           <div className="container mx-auto p-4">
-            <h3 className="text-5xl font-thin text-white">At Ravian, our vision is to redefine the frontiers of data science by integrating artificial intelligence with human ingenuity.</h3>
+            <h3 className="text-4xl sm:text-5xl font-thin text-white">At Ravian, our vision is to redefine the frontiers of data science by integrating artificial intelligence with human ingenuity.</h3>
             <p className="text-xl font-light text-white pt-10 w-11/12">
               We strive to develop AI-driven solutions that not only solve complex business problems but also cultivate a deeper understanding of the data that shapes our world. We envision a future where every organization, regardless of its size or sector, has access to the power of advanced data analytics, enabling them to make smarter decisions, innovate faster and drive meaningful progress. Our goal is to become the cornerstone for transformative analytics that empower business to lead with confidence and clarity.
             </p>
@@ -185,9 +212,9 @@ const App = () => {
         </div>
 
         <div className="about-section pt-40 " ref = {aboutRef} >
-          <h1 className="text-8xl font-semibold text-white font-poppins mb-10">About Us</h1>
+          <h1 className="text-6xl sm:text-8xl font-semibold text-white font-poppins mb-10">About Us</h1>
           <div className="container mx-auto p-4">
-            <h3 className="text-5xl font-thin text-white">At Ravian, we are dedicated to transforming the landscape of data analytics through innovation and cutting-edge technology</h3>
+            <h3 className="text-4xl sm:text-5xl font-thin text-white">At Ravian, we are dedicated to transforming the landscape of data analytics through innovation and cutting-edge technology</h3>
             <p className="text-xl font-light text-white pt-10 w-11/12">
             As pioneers in the field. we have developed the world's leading Al data scientist-an intelligent platform that leads the way in crafting machine learning and deep learning models, conducting comprehensive data analysis, and delivering actionable insights through intuitive visualizations.
             </p>
@@ -201,7 +228,7 @@ const App = () => {
         </div>
 
         <div className="cta-section pt-20 " ref = {ctaRef}>
-          <h1 className="text-8xl font-semibold text-white font-poppins mb-10">What are you waiting for?</h1>
+          <h1 className="text-7xl sm:text-8xl font-semibold text-white font-poppins mb-10">What are you waiting for?</h1>
           <button className="animated-button flex justify-center place-items-center font-poppins">Try Ravian</button>
         </div>
         
